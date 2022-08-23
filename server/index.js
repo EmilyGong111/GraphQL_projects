4c868665f6310e97135f3e7fb21e71376e77b410
Emily <mei.gong@icloud.com>
2022-08-23 11:25:42 +1000

    feat: server with graphql endpoint and schema

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {graphqlHTTP} = require("express-graphql");
+const schema = require("./schema/schema");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//play with the query at localhost:5000/graphql, query structure follow schema
+app.use("/graphql", graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === "development",
+}))
+
+app.listen(port, console.log(`server is running on ${port}`))
